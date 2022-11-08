b40d26ed864e60436475de353ac7935c49944711
dvirbs10 <dvir.bensimhon@mail.huji.ac.il>
2022-11-08 17:49:11 +0200

    part 2 hist

--- Ex_2.py
+++ Ex_2.py
@@ -3,10 +3,10 @@
 import math
 import random
 import matplotlib.pyplot as plt
+import numpy
 
 
 # Part A
-
 # Q 2a
 def initial_state(n, q, g):
     """
@@ -42,7 +42,6 @@ def two_solids(steps, n, g):
         if (i <= 100 and q_a_step < 150) or (
                 100 < i and 150 < q_a_step):  # if we move energy from A to B (i<100) and Solid A have les energy
             in_correct_energy_direction += 1
-
     g_A = g[:100]
     g_B = g[100:200]
     return g_A, g_B, q_A, q_B, in_correct_energy_direction
@@ -59,14 +58,33 @@ def energy_steps_graph(q1, q2, steps):
 
 
 # Part 2
-def bath_and_solid(steps, g):
-    """
+def hist(g, step, bins):
+    h = plt.hist(g, bins=range(bins))
+    plt.clf()
+    return h
 
+
+def plot_hist(h):
+    plt.plot(numpy.arange(20), numpy.array(h[0])/100)
+    plt.plot(numpy.arange(20), numpy.array(h[1])/100)
+    plt.plot(numpy.arange(20), numpy.array(h[2])/100)
+    plt.plot(numpy.arange(20), numpy.array(h[3])/100)
+    plt.plot(numpy.arange(20), numpy.array(h[4])/100)
+    plt.legend(['2*10^6', '4*10^6', '6*10^6', '8*10^6','10^7'])
+    plt.title("Numbers of Particles Each Energy histogram")
+    plt.xlabel('Energy Levels')
+    plt.ylabel('Numbers of Particles[Norm]')
+    plt.show()
+
+
+def bath_and_solid(steps, g, n_g):
+    """
     :return:
     """
     energy_g = list()
     energy_q23 = list()
-    for step in range(steps):
+    hist_different_steps = list()
+    for step in range(steps+1):
         i = random.randint(1, 100)
         delta = random.randrange(-1, 2, 2)
         if delta == -1:
@@ -79,14 +97,20 @@ def bath_and_solid(steps, g):
         tot_energy = sum(g)
         energy_g.append(tot_energy)
         energy_q23.append(g[23])
+        if step == 2 * 10 ** 6 or step == 4 * 10 ** 6 or step == 6 * 10 ** 6 or step == 8 * 10 ** 6 or step == 10 ** 7:
+            h = hist(g, step, 21)
+            hist_different_steps.append(h[0])
+    plot_hist(hist_different_steps)
     return g, energy_g, energy_q23
 
-def count_elements(seq) -> dict:
-    """Tally elements from `seq`."""
-    hist = {}
-    for i in seq:
-        hist[i] = hist.get(i, 0) + 1
-     return hist
+
+def energy_steps_graph_2(q, steps):
+    plt.plot([i for i in range(1, steps + 1)], q)
+    plt.xscale('log')
+    plt.xlabel('Log Step(time)')
+    plt.ylabel('Energy_Level')
+    plt.title("Change of Energy Level Solid")
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -101,12 +125,13 @@ if __name__ == '__main__':
     STEPS = 10 ** 5
     A, B, q_A, q_B, in_correct_energy_direction = two_solids(STEPS, N, AB)
     energy_steps_graph(q_A, q_B, STEPS)
-
+    print("in_correct_energy_direction", in_correct_energy_direction)
     # Part 2
     N_C = 100
     C = [0] * N_C
     theta = 2.5
+    steps = 10 ** 7
     STEPS = 10 ** 7
-    C, energy_C, energy_C23 = bath_and_solid(STEPS, C)
-
-
+    C, energy_C, energy_C23 = bath_and_solid(STEPS, C, N_C)
+    energy_steps_graph_2(energy_C, STEPS)
+    hist(energy_C23, STEPS, 25)
