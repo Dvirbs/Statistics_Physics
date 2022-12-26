0c4e930f2c94989d479dc56529b7be6ac405b979
dvirbs10 <dvir.bensimhon@mail.huji.ac.il>
2022-12-26 09:40:40 +0200

    26/12/2022 10:00

--- Ex4/Ex4_main.py
+++ Ex4/Ex4_main.py
@@ -1,49 +1,132 @@
 from Ex_4 import *
 import numpy as np
-import math
+import matplotlib.pyplot as plt
 
-for t in np.arange(T_min, T_max + 0.2, 0.2):
-    step = 0
+t_vector = np.arange(T_min, T_max, 0.2)
+N0_mean_per_t = np.zeros(np.size(t_vector))
+N0_squared_mean_per_t = np.zeros(np.size(t_vector))
+U_tot_mean = np.zeros(np.size(t_vector))
+c_v = np.zeros(np.size(t_vector))
+# print(N0_mean_per_t)
+
+for t_i, t in enumerate(t_vector):
     U = U_Levels()
     my = myu_binary_search_Step(N, n_max, t)
-    while step < 1:  # for now
-        # TODO change the step definition
-        N_i = random.randint(1, N) # random particle
-        bot_2_top = [str(math.floor(x)) + '-' + str(math.floor(y)) for x, y in zip(U.n_bottom, U.n_top)]
-        for i in range(0, len(bot_2_top), 10):
-            print(*bot_2_top[i:i + 10])
-        p_b = max(np.where(U.n_bottom <= N_i, U.n_bottom, U.n_bottom != 0))
-        p_b_i = np.where(p_b == U.n_bottom)[0][0]
-        part_level_i = np.where(N_i <= U.n_top)[0][0]
-        if part_level_i == p_b_i:
-            print('yay!!!')
-            print("p_b= ", p_b)
-            print("p_n= ", U.n_top[part_level_i])
+    # פרמטרים עבור ההתכנסות
+    k = 10 ** 6
+    k_2 = k * 2
+    N0_half_k = np.zeros(int(k * 0.5))
+    N0_full_k = np.zeros(int(k))
+    U_tot = np.zeros(int(k))
+    U_tot_sum = sum([(U.n_top[n] - U.n_bottom[n] + 1) * n for n in range(0, n_max - 1) if U.n_top[n] != 0])
+    U_last_temp = U_tot_sum
+    delta_cal = 1000
+    if t <= 1:
+        delta = 10 ** -3
+    elif 1 < t <= 2:
+        delta = 5 * (10 ** -3)
+    else:
+        delta = 10 ** -2
+    # delta = 1000 - 10
+    # delta_cal = random.randint(1, 1000)
+    step = 1
+
+    while delta <= delta_cal:
+        N_i = random.randint(1, N)  # random particle
+        # bot_2_top = [str(math.floor(x)) + '-' + str(math.floor(y)) for x, y in zip(U.n_bottom, U.n_top)]
+        # for i in range(0, len(bot_2_top), 10):
+        #     print(*bot_2_top[i:i + 10])
+        # p_b = max(np.where(U.n_bottom <= N_i, U.n_bottom, U.n_bottom != 0))
+        # p_b_i = np.where(p_b == U.n_bottom)[0][0]
+        # part_level_i = np.where(N_i <= U.n_top)[0][0]
+        part_level_i = np.argmax(U.n_top >= N_i)
+
+        # if part_level_i != p_b_i:
+        #      print('yay!!!')
+        #     print("p_b= ", p_b)
+        #     print("p_n= ", U.n_top[part_level_i], "part_level_i", part_level_i)
+        #     raise Exception("something wrong with the energy level")
         P_minus = energy_minus_probability(part_level_i, t, my)  # true or false for n-1
-        # TODO always P_minus=1?
         if P_minus:
-            U.n_top[part_level_i - 1] = U.n_bottom[part_level_i]
-            if U.n_bottom[part_level_i - 1] == 0:
-                U.n_bottom[part_level_i-1] = U.n_bottom[part_level_i]
-            if U.n_bottom[part_level_i] == U.n_top[part_level_i]:
-                U.n_bottom[part_level_i] = U.n_top[part_level_i] = 0
-            else:
-                U.n_bottom[part_level_i] += 1
+            if part_level_i != 0:
+                U_last_temp -= 1
+                U_tot_sum += U_last_temp
+                U.n_top[part_level_i - 1] = U.n_bottom[part_level_i]
+                if U.n_bottom[part_level_i - 1] == 0:
+                    U.n_bottom[part_level_i - 1] = U.n_bottom[part_level_i]
+                if U.n_bottom[part_level_i] == U.n_top[part_level_i]:
+                    U.n_bottom[part_level_i] = U.n_top[part_level_i] = 0
+                else:
+                    U.n_bottom[part_level_i] += 1
         else:
-            if part_level_i == 100:
-                continue
-            U.n_bottom[part_level_i + 1] = U.n_top[part_level_i]
-            if U.n_top[part_level_i + 1] == 0:
-                U.n_top[part_level_i + 1] = U.n_top[part_level_i]
+            if part_level_i != 99:  # python !!!!
+                U_last_temp += 1
+                U_tot_sum += U_last_temp
+                U.n_bottom[part_level_i + 1] = U.n_top[part_level_i]
+                if U.n_top[part_level_i + 1] == 0:
+                    U.n_top[part_level_i + 1] = U.n_top[part_level_i]
+                if U.n_top[part_level_i] == U.n_bottom[part_level_i]:
+                    U.n_bottom[part_level_i] = U.n_top[part_level_i] = 0
+                else:
+                    U.n_top[part_level_i] -= 1
+
+        # תנאיים עבור ההתנכסות
+        if 0.25 <= step / k_2 < 0.5:
             if U.n_top[part_level_i] == U.n_bottom[part_level_i]:
-                U.n_bottom[part_level_i] = U.n_top[part_level_i] = 0
+                N0_half_k[-(k - step)] = 1
             else:
-                U.n_top[part_level_i] -= 1
-
-
-        print("P_minus= ", P_minus)
-        print('bot_2_top_loop:')
-        bot_2_top = [str(math.floor(x)) + '-' + str(math.floor(y)) for x, y in zip(U.n_bottom, U.n_top)]
-        for i in range(0, len(bot_2_top), 10):
-            print(*bot_2_top[i:i + 10])
+                N0_half_k[-(k - step)] = U.n_top[part_level_i] - U.n_bottom[part_level_i]
+        elif 0.5 <= step / k_2 < 1:
+            if U.n_top[part_level_i] == U.n_bottom[part_level_i]:
+                N0_full_k[step - k] = 1
+            else:
+                N0_full_k[step - k] = U.n_top[part_level_i] - U.n_bottom[part_level_i]
+        elif k_2 / step == 1:
+            N0_full_k_mean = np.mean(N0_full_k)
+            delta_cal = abs(N0_full_k_mean - np.mean(N0_half_k)) / N0_full_k_mean
+            print("delta_cal", delta_cal)
+            if delta_cal <= delta:
+                print("t = ", t)
+                break
+            else:
+                k = k * 2
+                k_2 = k * 2
+                N0_half_k = N0_full_k
+                N0_full_k = np.zeros(int(k))
+                U_tot_sum = 0
+        if (step % 100000 == 0):
+            print("step = ", step, "temperature = ", t)
+        # print("t = ", t)
+        # print("k_2 = ", k_2, "step = ", step)
+        # print("P_minus= ", P_minus)
+        # print('bot_2_top_loop:')
+        # bot_2_top = [str(math.floor(x)) + '-' + str(math.floor(y)) for x, y in zip(U.n_bottom, U.n_top)]
+        # for i in range(0, len(bot_2_top), 10):
+        #     print(*bot_2_top[i:i + 10])
         step += 1
+    N0_mean_per_t[t_i] = np.mean(N0_full_k)
+    N0_squared_mean_per_t[t_i] = np.mean(N0_full_k ** 2)
+    U_tot_mean[t_i] = U_tot_sum / k
+    if step == k_2:
+        U_tot_mean[t_i] = U_tot_sum / k_2
+    c_v[t_i] = U_tot_mean[t_i] / (N * t)
+
+print(N0_mean_per_t)
+print(U_tot_mean)
+print(c_v)
+standard_deviation = (N0_squared_mean_per_t - N0_mean_per_t ** 2) ** 0.5
+print(standard_deviation)
+plt.plot(t_vector, N0_mean_per_t / N)
+plt.errorbar(t_vector, N0_mean_per_t / N, yerr=standard_deviation / N0_mean_per_t)
+# plt.xscale('log')
+plt.xlabel('temperature')
+plt.ylabel('<N0>/N')
+plt.title("<N_0>(T)/N for N = {}".format(N))
+plt.show()
+
+plt.plot(t_vector, c_v)
+plt.xlabel('temperature')
+plt.ylabel('c_v')
+plt.title("c_v(t) for N = {}".format(N))
+plt.show()
+
--- Ex4/Ex_4.py
+++ Ex4/Ex_4.py
@@ -6,10 +6,13 @@ import random
 
 
 class U_Levels:
-    """
-    """
 
+    g_calc = np.zeros(101)  # Caching of possible degredation values
     def __init__(self):
+
+        for n in range(0,101):
+            self.g_calc[n] = 0.5 * n * (n + 3) + 1
+
         U_store = self.U_initial_state()
         print('U_store', U_store)
         self.n_bottom = np.zeros(n_max)
@@ -21,8 +24,7 @@ class U_Levels:
                 self.n_top[n_i] = 0
             else:
                 self.n_bottom[n_i] = sum(U_store[:n_i]) + 1
-                self.n_top[n_i] = self.n_bottom[
-                                      n_i] + n - 1  # n_top is the highest particle at the n level. n = n_t - n_b + 1
+                self.n_top[n_i] = self.n_bottom[n_i] + n - 1  # n_top is the highest particle at the n level. n = n_t - n_b + 1
 
     def U_initial_state(self):
         U_store = np.zeros(n_max)
@@ -38,7 +40,15 @@ def myu_binary_search_Step(N, n_max, T):
     N_try = -1
     while int(N_try) != N:
         myu_try = 0.5 * (myu_min - myu_max) + myu_max
-        N_try = np.sum([g_calculation(n) / (np.exp((n - myu_try) / T) - 1) for n in range(0, n_max + 1)])
+
+        N_try = 0.0
+        for n in range(0, n_max + 1):
+            if (n - myu_try) / T < 50:
+                N_try += U_Levels.g_calc[n] / (np.exp((n - myu_try) / T) - 1)
+            else:
+                break
+
+#        N_try = np.sum([g_calculation(n) / (np.exp((n - myu_try) / T) - 1) for n in range(0, n_max + 1)])
         if N < N_try:
             myu_max = myu_try
         else:
@@ -46,22 +56,35 @@ def myu_binary_search_Step(N, n_max, T):
     return myu_try
 
 
-def g_calculation(n: int):
-    g = 0.5 * n * (n + 3) + 1
-    return g
-
+# def g_calculation(n: int):
+# #    g = 0.5 * n * (n + 3) + 1
+# #    return g
+#     return U_Levels.g_calc[n]
 
 def energy_minus_probability(n, t, myu):
     p = random.uniform(0, 1)
-    g_n_plus = g_calculation(n + 1)
-    g_n_minus = g_calculation(n - 1)
-    p_n_minus = (g_n_minus / (np.exp((n - 1 - myu) / t) - 1)) / (
-                g_n_plus / (np.exp((n + 1 - myu) / t) - 1) + g_n_minus / (np.exp((n - 1 - myu) / t) - 1))
+    g_n_plus = U_Levels.g_calc[n + 1]
+    g_n_minus = U_Levels.g_calc[n - 1]
     if n == 0:
-        g_n_zeros = g_calculation(n)
-        p_n_minus = (g_n_zeros / (np.exp((- myu) / t) - 1)) / (
-                g_n_plus / (np.exp((n + 1 - myu) / t) - 1) + g_n_zeros / (np.exp((- myu) / t) - 1))
-
+        threshold = 0.0098  # choose a small threshold value
+        if abs(- myu) / t < threshold:
+            # set p_n_minus to some default value (e.g., 0)
+            p_n_minus = 1
+        else:
+            # calculate p_n_minus as before
+            g_n_zeros = U_Levels.g_calc[n]
+            factor_gibbs_zero = np.exp((- myu) / t)
+            p_n_minus = (g_n_zeros / (factor_gibbs_zero - 1)) / (
+                    g_n_plus / (np.exp((1 - myu) / t) - 1) + g_n_zeros / (factor_gibbs_zero - 1))
+    else:
+        threshold = 0.0098  # choose a small threshold value
+        if abs(n - 1 - myu) / t < threshold:
+            # set p_n_minus to some default value (e.g., 0)
+            p_n_minus = 1
+        else:
+            factor_gibbs_n_minus_1 = np.exp((n - 1 - myu) / t)
+            p_n_minus = (g_n_minus / (factor_gibbs_n_minus_1 - 1)) / (
+                        g_n_plus / (np.exp((n + 1 - myu) / t) - 1) + g_n_minus / (factor_gibbs_n_minus_1 - 1))
     if p <= p_n_minus:
         energy_minus = 1
     else:
@@ -69,8 +92,8 @@ def energy_minus_probability(n, t, myu):
     return energy_minus
 
 
-if __name__ == '__main__':
-    myu = myu_binary_search_Step(1000, n_max, 0.2)
-    print("myu for N = 1000", myu)
-    myu = myu_binary_search_Step(10000, n_max, 0.2)
-    print("myu for N = 10000---Myu need to be smaller!", myu)
+# if __name__ == '__main__':
+#     myu = myu_binary_search_Step(1000, n_max, 0.2)
+#     print("myu for N = 1000", myu)
+#     myu = myu_binary_search_Step(10000, n_max, 0.2)
+#     print("myu for N = 10000---Myu need to be smaller!", myu)
